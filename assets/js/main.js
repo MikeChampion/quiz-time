f9f885226d91b0708b845018de5af5a073a20f36
Zathras <ChampionMike42@gmail.com>
2021-05-23 12:17:58 -0500

    Initial commit, UI fleshed out

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+/** QUIZ
+ * timer countdown
+ * display question
+ * 		multiple choice
+ * 		randomize the multiple choice
+
+ * if right score++
+ * if wrong decrement remaining time
+ * at game end 
+ * 		Enter initials
+ * 		record score and initials in local storage
+ * 		(High score list of top 5?)
+ *
+ *  */
+
+const questAnsList = [
+    {
+        question: "Which is not a basic data type?",
+        boolean: wrong,
+        undefined: wrong,
+        unknown: correct,
+        number: wrong,
+    },
+];
+
+/*
+document.getElementById("#timer");
+var questionCount = (document.getElemantByID("#question-count");
+"#prev-q-result"
+
+document.getElementById("#timer").textContent = countdown;
+*/
