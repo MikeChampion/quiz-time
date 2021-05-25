08a3be1bdd600f862fad62d5e446018388c3160a
Zathras <ChampionMike42@gmail.com>
2021-05-25 17:48:48 -0500

    Questions and answers populating with data attributes

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
  * timer countdown
  * display question
  * 		multiple choice
- * 		randomize the multiple choice
+ * 		randomize the multiple choice?
 
  * if right score++
  * if wrong decrement remaining time
@@ -19,34 +19,67 @@ const startBtnEl = document.getElementById("start-btn");
 const quizContainerEl = document.getElementById(
     "question-answer-result-container"
 );
+const questCountEl = document.getElementById("question-count");
 const questionEl = document.getElementById("question");
+const ansContEl = document.getElementById("answer-btn-container");
 const a1El = document.getElementById("a1");
+const a2El = document.getElementById("a2");
+const a3El = document.getElementById("a3");
+const a4El = document.getElementById("a4");
 const gameOverEl = document.getElementById("game-over-container");
 
-const questAnsList = [
+const questAnsArr = [
+    {
+        question: "Which of these is not a type of JavaScript?",
+        answers: [
+            { text: "ReactJS", correct: false },
+            { text: "VueJS", correct: false },
+            { text: "NodeJS", correct: false },
+            { text: "Java", correct: true },
+        ],
+    },
     {
         question: "Which is not a basic data type?",
-        boolean: "wrong",
-        undefined: "wrong",
-        unknown: "correct",
-        number: "wrong",
+        answers: [
+            { text: "undefined", correct: false },
+            { text: "unknown", correct: true },
+            { text: "boolean", correct: false },
+            { text: "number", correct: false },
+        ],
     },
 ];
+let ansLi = [a1El, a2El, a3El, a4El];
+let count = 0;
 let time = 10;
-let qCount = 0;
 
 function beginQuiz() {
     startBtnEl.classList.add("hidden");
     quizContainerEl.classList.remove("hidden");
     countdown();
-    renderQuestion();
+    pickQuestion();
 }
 
-function renderQuestion() {
-    for (qCount; qCount < questAnsList.length; qCount++) {
-        questionEl.textContent = questAnsList[qCount].value;
-        console.log(questAnsList[qCount].value);
+function pickQuestion() {
+    let i = count;
+    var qa = questAnsArr[i];
+    console.log(qa);
+    renderQuestAns(qa, i);
+
+    count++;
+}
+
+function renderQuestAns(question, i) {
+    questCountEl.textContent = i + 1;
+    questionEl.textContent = question.question;
+    let answers = question.answers;
+    for (let i = 0; i < answers.length; i++) {
+        ansLi[i].textContent = answers[i].text;
+        ansLi[i].dataset.correct = answers[i].correct;
     }
+}
+
+function chooseAnswer(e) {
+    //validate answer
 }
 
 function countdown() {
@@ -72,3 +105,5 @@ function countdown() {
 
 /* EVENT LISTENERS */
 startBtnEl.addEventListener("click", beginQuiz);
+
+ansContEl.addEventListener("click", checkAnswer);
