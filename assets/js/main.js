c829e29fbc9fb84aa97de30eea814c63a9135f93
Zathras <ChampionMike42@gmail.com>
2021-05-26 20:57:31 -0500

    Properly iterating through questions and capturing if the answer chosen is correct or not. Difficulty with the if/else to add to score or deduct time

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,9 @@
 /** QUIZ
- * timer countdown
- * display question
- * 		multiple choice
- * 		randomize the multiple choice?
+ * UI - basically DONE may need minor tweaks as progress is made
+ * timer countdown - DONE
+ * display question - DONE
+ * 		multiple choice - DONE
+ * 		randomize the multiple choice? 
 
  * if right score++
  * if wrong decrement remaining time
@@ -27,6 +28,7 @@ const a2El = document.getElementById("a2");
 const a3El = document.getElementById("a3");
 const a4El = document.getElementById("a4");
 const gameOverEl = document.getElementById("game-over-container");
+const prevAnsEl = document.getElementById("prev-q-result");
 
 const questAnsArr = [
     {
@@ -51,8 +53,15 @@ const questAnsArr = [
 let ansLi = [a1El, a2El, a3El, a4El];
 let count = 0;
 let time = 10;
+let prevAns = "";
+let score = 0;
 
 function beginQuiz() {
+    gameOverEl.classList.add("hidden");
+    count = 0;
+    time = 10;
+    prevAns = "";
+    score = 0;
     startBtnEl.classList.add("hidden");
     quizContainerEl.classList.remove("hidden");
     countdown();
@@ -60,11 +69,15 @@ function beginQuiz() {
 }
 
 function pickQuestion() {
+    console.log(score);
+    if (count === questAnsArr.length) {
+        endGame();
+        return;
+    }
     let i = count;
     var qa = questAnsArr[i];
     console.log(qa);
     renderQuestAns(qa, i);
-
     count++;
 }
 
@@ -76,10 +89,44 @@ function renderQuestAns(question, i) {
         ansLi[i].textContent = answers[i].text;
         ansLi[i].dataset.correct = answers[i].correct;
     }
+    if (prevAns === true) {
+        prevAnsEl.classList.remove("hidden");
+        prevAnsEl.textContent = "correct";
+    } else if (prevAns === false) {
+        prevAnsEl.classList.remove("hidden");
+        prevAnsEl.textContent = "wrong";
+    }
 }
 
 function chooseAnswer(e) {
-    //validate answer
+    const selectedButton = e.target;
+    const correct = selectedButton.dataset.correct;
+    // renderPrevAns(correct);
+    if (correct) {
+        score++;
+        console.log(score);
+    } else if (!correct) {
+        time--;
+        console.log("wrong answer");
+    }
+    pickQuestion();
+}
+
+function renderPrevAns(correct) {
+    console.log(correct);
+    correct ? score++ : time--;
+    // if (correct === true) {
+    //     score++;
+    //     console.log(score);
+    // } else {
+    //     time--;
+    //     console.log("wrong answer");
+    // }
+}
+
+function clearStatusClass(element) {
+    element.classlist.remove("correct");
+    element.classlist.remove("wrong");
 }
 
 function countdown() {
@@ -96,14 +143,19 @@ function countdown() {
 
         if (time === 0) {
             clearInterval(timeRemaining);
-            // startBtnEl.classList.remove("hidden");
-            // quizContainerEl.classList.add("hidden");
-            // gameOverEl.classList.remove("hidden");
+            endGame();
+            return;
         }
     }, 1000);
 }
 
+function endGame() {
+    startBtnEl.classList.remove("hidden");
+    quizContainerEl.classList.add("hidden");
+    gameOverEl.classList.remove("hidden");
+    alert("GAME OVER");
+}
+
 /* EVENT LISTENERS */
 startBtnEl.addEventListener("click", beginQuiz);
-
-ansContEl.addEventListener("click", checkAnswer);
+ansContEl.addEventListener("click", chooseAnswer);
